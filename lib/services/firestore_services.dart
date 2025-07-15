bc9ecbff7523df9a05ffec4a280af323b8a01b58
Mynul Alam <mynulalam27@gmail.com>
2025-07-16 01:31:36 +0600

    firestore services

--- lib/services/firestore_services.dart
+++ lib/services/firestore_services.dart
@@ -1,6 +1,7 @@
 import 'package:online_shopping/consts/firebase_const.dart';
 
 class FirestoreServices {
+  //get users data
   static getUser(uid) {
     return firestore
         .collection(usersCollection)
