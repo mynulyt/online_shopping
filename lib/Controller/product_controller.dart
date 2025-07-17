af0eb704d1330d0f88eeb1c3302d696262c9e4cf
Mynul Alam <mynulalam27@gmail.com>
2025-07-17 21:10:16 +0600

    product controller

--- lib/Controller/product_controller.dart
+++ lib/Controller/product_controller.dart
@@ -1,16 +1,15 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:online_shopping/Model/category_model.dart';
 
 class ProductController extends GetxController {
   var subcat = [];
-  getSubCategories() async {
+  getSubCategories(String title) async {
     var data = await rootBundle.loadString("lib/services/category_model.json");
     var decode = categoryModelFromJson(data);
 
     var s =
-        decode.categories.where((element) => element.name == Title).toList();
+        decode.categories.where((element) => element.name == title).toList();
 
     for (var e in s[0].subcategory) {
       subcat.add(e);
